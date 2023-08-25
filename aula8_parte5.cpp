b03aa0244bfe625aa0cb1a21551f57e7d204a715
Ayrton-R <107137970+Ayrton-R@users.noreply.github.com>
2023-08-24 22:22:14 -0400

    Add files via upload

---
+++ aula8_parte5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+float a, b, c;
+int main()
+{
+        cout<<"Digite o primeiro numero"<<endl;
+        cin>>a;
+        cout<<"Digite o segundo numero"<<endl;
+        cin>>b;
+        cout<<"Digite o terceiro numero"<<endl;
+        cin>>c;
+
+        if (a>b&& b>c){
+            cout<<a<<endl;
+        }
+        else if(b>a&& a>c)
+
+
+
+
+
+
+
+
+    return 0;
+}
