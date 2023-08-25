b48fad020e94c01c35a245f4e72641b636b4513d
Ayrton-R <107137970+Ayrton-R@users.noreply.github.com>
2023-08-24 22:09:08 -0400

    Add files via upload

---
+++ Aula8_parte3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+int a,b,resto;
+int main()
+{
+    cout<<"Informe o numero A"<<endl;
+    cin>>a;
+
+    cout<<"Informe o numero B"<<endl;
+    cin>>b;
+
+    resto=a%b;
+    if((resto)==0)
+    {
+        cout<<"Os numeros saum multiplus"<<endl;
+    }
+    else
+{
+    cout<<"Os numeros nao saum multiplus"<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
