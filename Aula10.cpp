bac5c915d216b492e2e01323b8d7e59b1f922bf5
Ayrton-R <107137970+Ayrton-R@users.noreply.github.com>
2023-08-31 22:27:21 -0400

    Add files via upload

---
+++ Aula10.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <locale.h>
+using namespace std;
+int counter =1;
+float  qtdn, media, nota, soma =0.0;
+int main()
+
+{
+    setlocale(LC_ALL, "Portuguese");
+    cout<<"Informe a quantidade de notas"<<endl;
+    cin>> qtdn;
+    while (counter <=qtdn){
+        cout<<"Informe uma nota:"<<counter<<endl;
+        cin>>nota;
+        soma = soma + nota;
+        counter++; //contador +1
+
+    }
+    media = soma/qtdn;
+    cout<<" A Média é:"<<media<<endl;
+
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ Aula10_parte1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#Include <locale.h>
+using namespace std;
+int n
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
